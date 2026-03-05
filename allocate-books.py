e13c0c99bd71c7fb857f8c0860fc9a8fd07153c2
Akanksha <a31695132@gmail.com>
2026-03-05 22:16:09 +0530

    DSA SERIES

--- allocate-books.py
+++ allocate-books.py
@@ -8,13 +8,9 @@ def can_allocate(arr, m, max_pages):
         else:
             students += 1
             current_sum = pages
-
             if students > m:
                 return False
-
     return True
-
-
 def allocate_books(arr, m):
     low = max(arr)
     high = sum(arr)
@@ -28,11 +24,8 @@ def allocate_books(arr, m):
             high = mid - 1
         else:
             low = mid + 1
-
     return answer
-
 
 arr = [12, 34, 67, 90]
 m = 2
-
 print("Minimum possible maximum pages:", allocate_books(arr, m))

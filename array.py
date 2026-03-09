91c7dfdf7b775a526c5b0f8a389d0be0ca911d27
Akanksha <a31695132@gmail.com>
2026-03-09 19:47:58 +0530

    DSA SERIES

---
+++ array.py
@@ -0,0 +1,12 @@
+rows = 3
+column = 3
+firstMatrix = []
+
+for i in range(rows):
+    rowArray = []
+    for j in range(column):
+        rowArray.append(j) # This adds the number 3
+    firstMatrix.append(rowArray)
+
+print(firstMatrix)
+
